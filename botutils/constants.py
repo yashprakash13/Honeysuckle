526d38f5a18a05fc685d1268117159b66ee23ed5
Yash Prakash <77504707+tintstY@users.noreply.github.com>
2021-06-26 22:47:37 +0530

    bot started, status done.

---
+++ botutils/constants.py
@@ -0,0 +1,9 @@
+import discord
+
+STATUS_ACTIVITY_DICT = {
+    '.helphs' : discord.ActivityType.listening,
+    'Love the way you link!' : discord.ActivityType.playing,
+    "You wish you had a mind like me, don't you?" : discord.ActivityType.playing, 
+    "It's OK. You're only human, after all." : discord.ActivityType.playing
+}
+
--- run_bot.py
+++ run_bot.py
@@ -5,12 +5,45 @@ import re
 from bs4 import BeautifulSoup
 import requests
 import os
-import cloudscraper
 import time
+import asyncio
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from discord import Embed, Colour
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
+from itertools import cycle
+
+from botutils.constants import *
 
 load_dotenv()  
 
+TOKEN = os.environ.get('DISCORD_TOKEN')
+
+bot = Bot(command_prefix='.')
+
+
+
+STATUSES = cycle(STATUS_ACTIVITY_DICT.keys())
+ACTIVITIES = cycle(STATUS_ACTIVITY_DICT.values())
+
+@tasks.loop(seconds=1)
+async def loop_bot_status():
+    """
+    Loop through all statuses for the bot every 13 sec
+    """
+
+    await bot.wait_until_ready()
+
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=next(ACTIVITIES),
+            name=(next(STATUSES)).strip()
+        )
+    )
+
+    await asyncio.sleep(13)
+
+
+loop_bot_status.start()
+bot.run(TOKEN)
