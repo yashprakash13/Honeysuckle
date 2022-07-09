060fdb5e793266ec01abd43a0e6ff7d050048683
Yash Prakash <yashprakash13@gmail.com>
2022-07-09 14:33:27 -0400

    fixed status message.

--- botutils/constants.py
+++ botutils/constants.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 STATUS_ACTIVITY_DICT = {
-    '.helphs' : discord.ActivityType.listening,
+    '.help' : discord.ActivityType.listening,
     'Love the way you link!' : discord.ActivityType.playing,
     "You wish you had a mind like me, don't you?" : discord.ActivityType.playing, 
     "It's OK. You're only human, after all." : discord.ActivityType.playing
